92d700cb2db2103e388c68976a870d3a7d201921
Danny-McNaught <sfcsarge@outlook.com>
2024-02-14 16:43:14 -0500

    Run, and set zero working now, still need GoHome button to work.

--- Stepper/MainWindow.xaml.cs
+++ Stepper/MainWindow.xaml.cs
@@ -1,12 +1,8 @@
 ﻿using System.Windows;
-using UtilityDelta.Gpio.Implementation;
-using UtilityDelta.Stepper;
 using System.IO.Ports;
-using System.Text.RegularExpressions;
 
 namespace Stepper
 {
-    using System.Text.RegularExpressions;
     using System.Globalization;
     using System.Windows.Controls;
     using System.Windows.Input;
@@ -18,8 +14,13 @@ namespace Stepper
     public partial class MainWindow : MetroWindow
     {
         public SerialPort sp = new SerialPort();
-        public string myPortName = "COM5"; // Serial Port Name
+        public string myPortName; // Serial Port Name
         public string stringValue = "";
+        public int zeroXaxis = 0;
+        public int zeroYaxis = 0;
+        public int baudRate = 9600;
+        public int milliseconds = 4000;
+
         private void TextBox_OnPreviewTextInput(object sender, TextCompositionEventArgs e)
         {
             var textBox = sender as TextBox;
@@ -37,35 +38,122 @@ namespace Stepper
         public MainWindow()
         {
             InitializeComponent();
-            myPortName = cmbComPort.Text;
-            int baudRate = Convert.ToInt32(txtBaudRate.Text);
+            try
+            {
+                txtBaudRate.Text = "9600";
+                baudRate = Convert.ToInt32(txtBaudRate.Text);
+                cmbComPort.SelectedValue = "COM3";
+                myPortName = "COM3";
+                sp = new(myPortName, baudRate);
+                if (sp.IsOpen == false) { sp.Open(); }
+            }
+            catch (System.IO.IOException ex)
+            {
+                string error = ex.Message;
+                if (error.Contains("COM3"))
+                {
+                txtBaudRate.Text = "9600";
+            baudRate = Convert.ToInt32(txtBaudRate.Text);
+                    cmbComPort.SelectedValue = "COM5";
+                    myPortName = "COM5";
             sp = new(myPortName, baudRate);
-            sp.Open();
+            if (sp.IsOpen == false) { sp.Open(); }
+        }
+            }
         }
         private void btnRun_Click(object sender, RoutedEventArgs e)
         {
-            stringValue = txtStepperXMove.Text.Trim() + "," + txtStepperYMove.Text.Trim() + "," + txtMotorSpeed.Text.Trim() + "," + txtMotorAcceleration.Text.Trim() + ",";
+            if (ckbXaxisResetToZero.IsChecked == true && ckbYaxisResetToZero.IsChecked == true)
+            {
+                zeroXaxis = 1;
+                zeroYaxis = 1;
+                stringValue = txtXaxisStepperMove.Text.Trim() + "," + txtXaxisMotorSpeed.Text.Trim() + "," + zeroXaxis.ToString() + "," + txtYaxisStepperMove.Text.Trim() + "," + txtYaxisMotorSpeed.Text.Trim() + "," + zeroYaxis.ToString() + ",";
+                sp.Write(stringValue);
+                ckbXaxisResetToZero.IsChecked = false;
+                txtXaxisStepperMove.Text = "0.00";
+                ckbYaxisResetToZero.IsChecked = false;
+                txtYaxisStepperMove.Text = "0.00";
+                stringValue = txtXaxisStepperMove.Text.Trim() + "," + txtXaxisMotorSpeed.Text.Trim() + "," + zeroXaxis.ToString() + "," + txtYaxisStepperMove.Text.Trim() + "," + txtYaxisMotorSpeed.Text.Trim() + "," + zeroYaxis.ToString() + ",";
+                sp.Write(stringValue);
+                zeroXaxis = 0;
+                zeroYaxis = 0;
+            }
+            else if (ckbXaxisResetToZero.IsChecked == true && ckbYaxisResetToZero.IsChecked == false)
+            {
+                zeroXaxis = 1;
+                zeroYaxis = 0;
+                stringValue = txtXaxisStepperMove.Text.Trim() + "," + txtXaxisMotorSpeed.Text.Trim() + "," + zeroXaxis.ToString() + "," + txtYaxisStepperMove.Text.Trim() + "," + txtYaxisMotorSpeed.Text.Trim() + "," + zeroYaxis.ToString() + ",";
             sp.Write(stringValue);
+                ckbXaxisResetToZero.IsChecked = false;
+                txtXaxisStepperMove.Text = "0.00";
+                stringValue = txtXaxisStepperMove.Text.Trim() + "," + txtXaxisMotorSpeed.Text.Trim() + "," + zeroXaxis.ToString() + "," + txtYaxisStepperMove.Text.Trim() + "," + txtYaxisMotorSpeed.Text.Trim() + "," + zeroYaxis.ToString() + ",";
+                sp.Write(stringValue);
+                zeroXaxis = 0;
+                zeroYaxis = 0;
+            }
+            else if (ckbXaxisResetToZero.IsChecked == false && ckbYaxisResetToZero.IsChecked == true)
+            {
+                zeroXaxis = 0;
+                zeroYaxis = 1;
+                stringValue = txtXaxisStepperMove.Text.Trim() + "," + txtXaxisMotorSpeed.Text.Trim() + "," + zeroXaxis.ToString() + "," + txtYaxisStepperMove.Text.Trim() + "," + txtYaxisMotorSpeed.Text.Trim() + "," + zeroYaxis.ToString() + ",";
+                sp.Write(stringValue);
+                ckbYaxisResetToZero.IsChecked = false;
+                txtYaxisStepperMove.Text = "0.00";
+                stringValue = txtXaxisStepperMove.Text.Trim() + "," + txtXaxisMotorSpeed.Text.Trim() + "," + zeroXaxis.ToString() + "," + txtYaxisStepperMove.Text.Trim() + "," + txtYaxisMotorSpeed.Text.Trim() + "," + zeroYaxis.ToString() + ",";
+                sp.Write(stringValue);
+                zeroXaxis = 0;
+                zeroYaxis = 0;
+            }
+            if (ckbXaxisResetToZero.IsChecked == false && ckbYaxisResetToZero.IsChecked == false)
+            {
+                stringValue = txtXaxisStepperMove.Text.Trim() + "," + txtXaxisMotorSpeed.Text.Trim() + "," + zeroXaxis.ToString() + "," + txtYaxisStepperMove.Text.Trim() + "," + txtYaxisMotorSpeed.Text.Trim() + "," + zeroYaxis.ToString() + ",";
+                sp.Write(stringValue);
+            }
         }
         private void btnStop_Click(object sender, RoutedEventArgs e)
         {
-            txtStepperXMove.Text = "0.00";
-            txtStepperYMove.Text = "0.00";
-            stringValue = txtStepperXMove.Text.Trim() + "," + txtStepperYMove.Text.Trim() + "," + txtMotorSpeed.Text.Trim() + "," + txtMotorAcceleration.Text.Trim() + ",";
+            txtXaxisStepperMove.Text = "0.00";
+            txtYaxisStepperMove.Text = "0.00";
+            if (ckbXaxisResetToZero.IsChecked == true)
+            {
+                ckbXaxisResetToZero.IsChecked = false;
+                zeroXaxis = 0;
+            }
+            if (ckbYaxisResetToZero.IsChecked == true)
+            {
+                ckbYaxisResetToZero.IsChecked = false;
+                zeroYaxis = 0;
+            }
+            stringValue = txtXaxisStepperMove.Text.Trim() + "," + txtXaxisMotorSpeed.Text.Trim() + zeroXaxis.ToString() + "," + txtYaxisStepperMove.Text.Trim() + "," + txtYaxisMotorSpeed.Text.Trim() + zeroYaxis.ToString() + ",";
             sp.Write(stringValue);
         }
         protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
         {
-            sp.Close();
+            if (sp.IsOpen == true) { sp.Close(); }
             base.OnClosing(e);
         }
 
         private void btnGoHome_Click(object sender, RoutedEventArgs e)
         {
-            txtStepperXMove.Text = "0.00";
-            txtStepperYMove.Text = "0.00";
-            stringValue = txtStepperXMove.Text.Trim() + "," + txtStepperYMove.Text.Trim() + "," + txtMotorSpeed.Text.Trim() + "," + txtMotorAcceleration.Text.Trim() + ",";
+            txtXaxisStepperMove.Text = "0.00";
+            ckbXaxisResetToZero.IsChecked = false;
+            zeroXaxis = 0;
+            txtYaxisStepperMove.Text = "0.00";
+            ckbYaxisResetToZero.IsChecked = false;
+            zeroYaxis = 0;
+            stringValue = txtXaxisStepperMove.Text.Trim() + "," + txtXaxisMotorSpeed.Text.Trim() + zeroXaxis.ToString() + "," + txtYaxisStepperMove.Text.Trim() + "," + txtYaxisMotorSpeed.Text.Trim() + zeroYaxis.ToString() + ",";
             sp.Write(stringValue);
+        }
+        private void CheckBoxChanged(object sender, RoutedEventArgs e)
+        {
+            if (ckbXaxisResetToZero.IsChecked == true)
+            {
+                zeroXaxis = 0;
+            }
+            if (ckbYaxisResetToZero.IsChecked == true)
+            {
+                zeroYaxis = 0;
+            }
         }
     }
 }
